7dcd9df8ca1f7c0d09e3017117792c7b786eb3cc
Zain Malik <116706625+zainmalik33543@users.noreply.github.com>
2025-03-18 09:55:38 +0500

    Update counterSlice.js

--- src/lib/slices/counterSlice.js
+++ src/lib/slices/counterSlice.js
@@ -4,7 +4,6 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   value: 0,
 };
-
 const counterSlice = createSlice({
   name: "counter",
   initialState,
@@ -15,12 +14,10 @@ const counterSlice = createSlice({
     decrement: (state) => {
       state.value -= 1;
     },
-    
     incrementByAmount: (state, action) => {
       state.value += action.payload;
     },
   },
 });
-
 export const { increment, decrement, incrementByAmount } = counterSlice.actions;
 export default counterSlice.reducer;

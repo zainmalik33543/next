138af840b61b06d2792320dd813a4872008c212d
Zain Malik <116706625+zainmalik33543@users.noreply.github.com>
2025-03-18 09:56:58 +0500

    Update Post.js

--- src/lib/models/Post.js
+++ src/lib/models/Post.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
 
-const PostSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  content: { type: String, required: true },
-}, { timestamps: true });
+const PostSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
 export default mongoose.models.Post || mongoose.model("Post", PostSchema);

c029aa4c409a92bc16e6a57d40e5613ac2da995e
Zain Malik <116706625+zainmalik33543@users.noreply.github.com>
2025-03-18 09:56:15 +0500

    Update page.js

--- src/app/blogs/[id]/page.js
+++ src/app/blogs/[id]/page.js
@@ -1,14 +1,19 @@
 import Link from "next/link";
 
 export default async function BlogPost({ params }) {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.id}`);
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${params.id}`
+  );
   const post = await res.json();
 
   return (
     <div style={{ padding: "20px", maxWidth: "600px", margin: "auto" }}>
       <h1>{post.title}</h1>
       <p>{post.body}</p>
-      <Link href="/blogs" style={{ color: "blue", textDecoration: "underline" }}>
+      <Link
+        href="/blogs"
+        style={{ color: "blue", textDecoration: "underline" }}
+      >
         ⬅ Back to Blogs
       </Link>
     </div>

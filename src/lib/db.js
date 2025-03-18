fe255b93c0c4058629eb6b407e925739aff6035e
Zain Malik <116706625+zainmalik33543@users.noreply.github.com>
2025-03-18 09:56:33 +0500

    Update db.js

--- src/lib/db.js
+++ src/lib/db.js
@@ -4,7 +4,6 @@ export async function connectDB() {
   if (mongoose.connection.readyState >= 1) {
     return; // Already connected
   }
-  
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("✅ Connected to MongoDB");

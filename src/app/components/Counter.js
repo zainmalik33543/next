1810df84427a13725cf72061d77fd4ebd057abca
Zain Malik <116706625+zainmalik33543@users.noreply.github.com>
2025-03-18 09:57:51 +0500

    Update Counter.js

--- src/app/components/Counter.js
+++ src/app/components/Counter.js
@@ -1,13 +1,11 @@
 // app/components/Counter.js
 "use client";
-
 import { useSelector, useDispatch } from "react-redux";
 import { increment, decrement } from "@/lib/slices/counterSlice";
 
 export default function Counter() {
   const count = useSelector((state) => state.counter.value);
   const dispatch = useDispatch();
-
   return (
     <div>
       <h2>Count: {count}</h2>

bc0411a515954c226e71560d64e493f395f11106
Zain Malik <116706625+zainmalik33543@users.noreply.github.com>
2025-04-07 15:54:12 +0500

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ReduxProvider from "@/lib/store/Provider";
 
+
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
